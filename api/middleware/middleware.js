2798c722ae7468be7732bfc92be5dfdab9b6b7eb
ruizaj13 <ruizaj13@gmail.com>
2021-01-14 15:20:29 -0800

    get projects by id route complete with proper middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,4 +1,18 @@
-// const Proj = require('../projects/projects-model')
-// const Act = require('../actions/actions-model')
+const Proj = require('../projects/projects-model')
+const Act = require('../actions/actions-model')
 
-// module.exports = {}
+async function valPostId(req, res, next) {
+    try {
+        const prjct = await Proj.get(req.params.id)
+        if (prjct) {
+            req.prjct = prjct
+            next()
+        } else {
+            res.status(404).json(`User with id ${req.params.id} not found`)
+        }
+    } catch (err) {
+        res.status(500).json('Something has gone terribly wrong')
+    }
+}
+
+module.exports = {valPostId}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Proj = require('../projects/projects-model')
+const {valPostId} = require('../middleware/middleware')
 
 router.get('/', (req, res, next) => {
     Proj.get()
@@ -10,6 +11,10 @@ router.get('/', (req, res, next) => {
         .catch(err => {
             next(err)
         })
+})
+
+router.get('/:id', valPostId, (req, res) => {
+    res.status(200).json(req.prjct)
 })
 
 
