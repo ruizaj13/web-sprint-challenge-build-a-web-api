3d43a453125c29c99262f4174b639ecf5c8f27bb
ruizaj13 <ruizaj13@gmail.com>
2021-01-14 14:37:43 -0800

    server.js basics done

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const server = express();
 
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Juan's sprint challenge</h1>`);
+  });
 
 module.exports = server;
